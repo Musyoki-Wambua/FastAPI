56ae2e40507d517123ac1d1b205a6505421885a6
Musyoki-Wambua <wjosephmusyoki@gmail.com>
2023-11-14 12:19:16 +0300

    Hash and Authenticating Password

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from datetime import datetime, timedelta
 from jose import JWTError, jwt
 from passlib.context import CryptContext
 
-SECRET_KEY = "c286dbb2a92035c48de744a5462bb6350527acc0f3690a3dee9e2648b98d8f8a"
+SECRET_KEY = ""
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
@@ -39,3 +39,35 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
     
 app = FastAPI()
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+def get_user(db, username: str):
+    if username in db: 
+        user_data = db[username]
+        return UserInDB(**user_data)
+
+def authenticate_user(db, username: str, password: str):
+    user = get_user(db, username) 
+    if not user: 
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False 
+    
+    return user 
+
+def create_access_token(data: dict, expires_delta: timedelta or None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta 
+    else:
+        expire = datetime.utcnow() + timedelta(minutes = 15)
+        
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+ 
