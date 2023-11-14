7eec8691267aa8e47119347e3bde6da433598e2c
Musyoki-Wambua <wjosephmusyoki@gmail.com>
2023-11-14 09:33:41 +0300

    Setting up API user Authentication

--- main.py
+++ main.py
@@ -1,16 +1,41 @@
 from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from pydantic import BaseModel
+from datetime import datetime, timedelta
+from jose import JWTError, jwt
+from passlib.context import CryptContext
 
+SECRET_KEY = "c286dbb2a92035c48de744a5462bb6350527acc0f3690a3dee9e2648b98d8f8a"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+fake_db = {
+    "joe": {
+        "name": "John",
+        "full_name": "Musyoki Wambua",
+        "email": "joe@gmail.com",
+        "hashed_password": "$pbkdf2-sha256$2900",
+        "disabled": False
+    }
+}
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+    
+class TokenData(BaseModel):
+    username: str or None = None
+    
+class User(BaseModel):
+    username: str
+    email: str or None = None
+    full_name: str or None = None
+    disabled: bool or None = None
+    
+class UserInDB(User):
+    hashed_password: str
+    
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+    
 app = FastAPI()
-
-class Data(BaseModel):
-    name: str
-
-@app.post("/create/")
-async def create(data: Data):
-    return {"data": data}
-
-@app.get("/test/{item_id}")
-async def test(item_id: str, query: int = 1):
-    return {"hello": item_id}
